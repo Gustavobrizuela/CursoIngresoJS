36f8bcdf401cd8d514f0b62dc18a75be6af1c7e0
Gustavo Brizuela <Gustybrizuela2606@gmail.com>
2020-07-13 20:21:54 -0300

    ejercicios de la 1er clase

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -3,7 +3,9 @@ Debemos lograr tomar un dato por 'PROMPT'
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
-	alert("ok");
+{ 
+	let Nombre;
+	Nombre=prompt("Ingrese su nombre");
+	document.getElementById("txtIdNombre").value=Nombre;
 }
 
