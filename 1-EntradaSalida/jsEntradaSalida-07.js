f110aa6c032f241524443d8b7c55af573e39322e
Gustavo Brizuela <Gustybrizuela2606@gmail.com>
2020-07-14 22:54:41 -0300

    Todos los ejercicios completados

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -7,20 +7,16 @@ function sumar()
 {	
 	//Sumar y restar hice de 1 manera ,multiplicar y dividir hice otra manera (ejemplos);
 	let numero1;
-	let numero2;
-	let resultado;	
-
-	numero1= document.getElementById("txtIdNumeroUno").value;
 	
-	numero1=parseInt(numero1);
+	let numero2;
 
-	numero2= document.getElementById("txtIdNumeroDos").value;
+	let resultado;
 
-	numero2=parseInt(numero2);
+	numero1= parseInt (document.getElementById("txtIdNumeroUno").value);
+	numero2= parseInt (document.getElementById("txtIdNumeroDos").value);
+    resultado = numero1 + numero2;
 
-	resultado = numero1 + numero2;
-
-	alert("La suma es " + resultado);	
+	alert (" la suma es " + resultado);
 }
 
 function restar()
